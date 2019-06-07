5a218b057d67d13ea8959985b6ca29d45cd3df76
daggag <daggage2017@gmail.com>
2019-06-07 10:14:40 -0700

    remove table from categories

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,13 +1,3 @@
 <div class="container">
     <h1>Categories</h1>
-    <table class="table table-bordered">
-        <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Control</th>
-        </tr>
-        </thead>
-    </table>
-</div>
+    </div>
