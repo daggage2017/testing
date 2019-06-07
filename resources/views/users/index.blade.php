aabdeadc8c94a95ab9d214ccc072127a2a2e4cbf
daggag <daggage2017@gmail.com>
2019-06-07 07:57:05 -0700

    edit user add tale

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,6 +1,13 @@
 <div class="container">
     <h1>Users Page</h1>
     <table class="table table-bordered">
-
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>User Name</th>
+            <th>Control</th>
+        </tr>
+        </thead>
     </table>
 </div>
